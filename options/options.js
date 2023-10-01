9a8480fe7c3280458cf60c4663a64e6f0bd9d920
Crowdbotics <team@crowdbotics.com>
2023-10-01 21:20:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-wind-43694",
+  url: "https://tight-wind-43694.botics.co",
+  api: "https://tight-wind-43694.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
